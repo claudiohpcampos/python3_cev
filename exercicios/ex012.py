3d8e2c7c255d927da1e9c3237037d9d5c60a3a4f
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-07-29 17:33:11 -0300

    Create ex012.py
    Calculando desconto

---
+++ exercicios/ex012.py
@@ -0,0 +1,2 @@
+prod = float(input('Qual é o preço do produto? R$'))
+print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}.'.format(prod, prod - (prod * 5 / 100)))
