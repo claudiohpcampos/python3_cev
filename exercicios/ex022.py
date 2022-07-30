8f4c9198350ec31dc87c914751c9d44da1c6f766
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-07-30 01:54:31 -0300

    Create ex022.py
    Analisador de textos

---
+++ exercicios/ex022.py
@@ -0,0 +1,13 @@
+from time import sleep
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+sleep(1)
+print('Seu nome em maiúsculas: {}'.format(nome).upper())
+sleep(1)
+print('Seu nome em minúsculas: {}'.format(nome).lower())
+sleep(1)
+print('Seu nome tem ao todo {} letras.'.format(len(nome)-nome.count(' ')))
+sleep(1)
+print('Seu primeiro nome tem {} letras.'.format(nome.find(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras.'.format(separa[0], len(separa[0])))
