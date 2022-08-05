47dcc5a0b235fc75dcf1eb5bed71fd935d708373
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-08-05 11:13:03 -0300

    Create ex046.py

---
+++ exercicios/ex046.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+
+from time import sleep
+import emoji
+print('=' * 5, 'Contagem Regressiva', '=' * 5)
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print(emoji.emojize(':tada: VIVA! :tada:', language='alias'))
