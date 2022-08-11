d07b21fa58779a6227f7ed9064b7b34fa8bb05ba
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-08-11 14:43:07 -0300

    Create ex063.py

---
+++ exercicios/ex063.py
@@ -0,0 +1,13 @@
+print('{:-^31}'.format(' Sequência de Fibronacci '))
+num = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+cont = 3
+print(f'{t1} -> {t2}', end='')
+while cont <= num:
+    t3 = t1 + t2
+    print(f' -> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' -> FIM')   
